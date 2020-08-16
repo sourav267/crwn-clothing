5b7cb422b4918d7d89147abe582bbe3c427dff10
sourav13 <souravkumar267@gmail.com>
2020-08-16 23:20:25 +0530

    added remove from cart-item fuctionality

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -3,9 +3,24 @@ export const addItemToCart = (cartItems, cartItemsToAdd) => {
         cartItem => cartItem.id === cartItemsToAdd.id);
     if (existingCartItem) {
         return cartItems.map(cartItem =>
-            cartItem.id !== cartItemsToAdd.id
+            cartItem.id === cartItemsToAdd.id
                 ? { ...cartItem, quantity: cartItem.quantity + 1 }
                 : cartItem);
     }
     return [...cartItems, { ...cartItemsToAdd, quantity: 1 }]
+};
+
+export const removeItemFromCart = (cartItems, cartItemsToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemsToRemove.id);
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem =>
+            cartItem.id !== cartItemsToRemove.id)
+    }
+    return cartItems.map(
+        cartItem =>
+            cartItem.id === cartItemsToRemove.id ?
+                { ...cartItem, quantity: cartItem.quantity - 1 }
+                : cartItem
+    )
 };
